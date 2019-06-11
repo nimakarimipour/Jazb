bfa4ee071ba778039c0443e649bebd5435f0486e
Nima Karimipour <karimipour.nima@gmail.com>
2019-06-11 23:57:04 +0430

    Bug Fix

--- jazb/lib/product_control.dart
+++ jazb/lib/product_control.dart
@@ -10,7 +10,7 @@ class ProductControl extends StatelessWidget {
       return RaisedButton(
             color: Theme.of(context).primaryColor,
             onPressed: () {
-              addProduct({'title': 'Chocolate', 'image': 'assets/food.jpg'});
+              addProduct({'title': 'Narenjestan', 'image': 'assets/restaurant_interior_1.jpg'});
             },
             child: Text('Add Product'),
           );
