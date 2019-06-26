0b0736fcb1a288f4226f9a58482606128c578d36
Nima Karimipour <karimipour.nima@gmail.com>
2019-06-27 02:31:45 +0430

    Simplify

--- jazb/lib/main.dart
+++ jazb/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:jazb/pages/category.dart';
-
-import './pages/auth.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
